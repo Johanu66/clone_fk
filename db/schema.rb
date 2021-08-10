100b1b2d48f7c8c7baa234aff237b3190993db18
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-10 21:19:10 +0100

    Preparation of the working environment

---
+++ db/schema.rb
@@ -0,0 +1,22 @@
+ActiveRecord::Schema.define(version: 2021_08_10_195343) do
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
+end
