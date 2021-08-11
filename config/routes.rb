99e302589a35ce5153a63a569eb76b95bb6e17e1
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-11 10:55:57 +0100

    The registration and login functionalities are ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
+  post '/users/new', to: 'users#create'
   resources :users
+  get '/sessions/new', to: 'sessions#new', as: 'new_session'
+  post '/sessions/new' , to: 'sessions#create', as: 'create_session'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
