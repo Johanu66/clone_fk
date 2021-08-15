3df781160249383d665ae8e996c100b32b9ada08
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-15 04:37:43 +0100

    Page 404 et 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  get '/', to: 'posts#index'
   post '/posts/new', to: 'posts#confirm', as: 'confirm_post'
   post '/users/new', to: 'users#create'
   resources :users
