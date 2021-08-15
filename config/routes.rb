119433338a1e7be8d7b8f136c2d7ef0aa0a2a5af
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-15 02:21:19 +0100

    Fontionnalité d'affichage et de suppression de publication(post)

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
   get '/sessions/new', to: 'sessions#new', as: 'new_session'
   post '/sessions/new' , to: 'sessions#create', as: 'create_session'
+  get '/sessions/destroy' , to: 'sessions#destroy', as: 'destroy_session'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
