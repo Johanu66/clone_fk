774a359b2f6e5867a6e60f5e93a78da39a37fea9
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-15 03:33:52 +0100

    Revue du design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,13 +65,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :content, :photo, :photo_cache, :user_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :content, :photo, :photo_cache, :user_id)
+  end
 end
