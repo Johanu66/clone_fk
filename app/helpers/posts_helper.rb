f70109a8e475d2cc8c6acd6fa142a42a1396cbc5
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-08-15 02:58:31 +0100

    Fontionnalité de modification de publication(post)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+    def submit_text
+        if action_name == 'new'
+            "Créer un compte"
+        else
+            "Enregistrer les modifications"
+        end
+    end
+    def form_url
+        if action_name == 'new'
+            confirm_post_path
+        else
+            post_path
+        end
+    end
 end
